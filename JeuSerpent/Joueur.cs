a43b148f991cdfd3ab07330d494d0a1d91f9e207
Seynabou BA KANE <seynabou.bk@gmail.com>
2024-12-11 15:04:51 +0100

    Mise à jour du .gitignore et initialisation jeu du serpent

---
+++ JeuSerpent/Joueur.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuSerpent
+{
+    internal class Joueur
+    {
+        public string Nom { get; private set; }
+        public int Position { get; private set; }
+        public Joueur(String nom) 
+        {
+            Nom = nom;
+            Position = 0;
+        }
+
+        public void AfficherInfos() 
+        { 
+            Console.WriteLine($"Joueur: {Nom}, Position: {Position}");
+        }
+    }
+}
---
+++ JeuSerpent/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JeuSerpent;
+
+namespace JeuSerpent
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Entrez le nom du premier joureur");
+            string? InputJoueur1 = Console.ReadLine();
+            string NomJoueur1 = InputJoueur1 ?? "Joueur 1";
+
+            Console.WriteLine("Entrez le nom du deuxieme joureur");
+            string? InputJoueur2 = Console.ReadLine();
+            string NomJoueur2 = InputJoueur2 ?? "Joueur 2";
+
+            Joueur Joueur1 = new(NomJoueur1);
+            Joueur Joueur2 = new(NomJoueur2);
+
+            Console.WriteLine("\nJoueurs initialisés : ");
+            Joueur1.AfficherInfos();
+            Joueur2.AfficherInfos();
+        }
+
+    }
+}
