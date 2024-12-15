8f1221533575b3b50352c53b1627f1451c35ca48
Seynabou BA KANE <seynabou.bk@gmail.com>
2024-12-15 14:48:50 +0100

    Ajout du jeu du serpent complet exo 1

--- JeuSerpent/Joueur.cs
+++ JeuSerpent/Joueur.cs
@@ -6,19 +6,35 @@ using System.Threading.Tasks;
 
 namespace JeuSerpent
 {
-    internal class Joueur
+    internal class Joueur(String nom)
     {
-        public string Nom { get; private set; }
-        public int Position { get; private set; }
-        public Joueur(String nom) 
-        {
-            Nom = nom;
-            Position = 0;
-        }
+        public string Nom { get; private set; } = nom;
+        public int Position { get; private set; } = 0;
 
         public void AfficherInfos() 
         { 
             Console.WriteLine($"Joueur: {Nom}, Position: {Position}");
         }
+
+        public static int LancerDe()
+        {
+            Random random = new();
+            int ValeurDe = random.Next(1, 7);
+            return ValeurDe;
+        }
+
+        public void Deplacer(int ValeurDe1)
+        {
+            Position += ValeurDe1;
+
+            if (Position > 50)
+            {
+                Console.Write($"{Nom} depasse la case 50 et revient à la case 25 ! ");
+                Position = 25;
+            }
+            Console.WriteLine($"{Nom} se deplace à la position {Position} ! ");
+
+        }
+
     }
 }
--- JeuSerpent/Program.cs
+++ JeuSerpent/Program.cs
@@ -5,27 +5,61 @@ using System.Text;
 using System.Threading.Tasks;
 using JeuSerpent;
 
-namespace JeuSerpent
+Console.WriteLine("Entrez le nom du premier joureur");
+string? InputJoueur1 = Console.ReadLine();
+string NomJoueur1 = InputJoueur1 ?? "Joueur 1";
+
+Console.WriteLine("Entrez le nom du deuxieme joureur");
+string? InputJoueur2 = Console.ReadLine();
+string NomJoueur2 = InputJoueur2 ?? "Joueur 2";
+
+Joueur Joueur1 = new(NomJoueur1);
+Joueur Joueur2 = new(NomJoueur2);
+
+Console.WriteLine("\nJoueurs initialisés : ");
+Joueur1.AfficherInfos();
+Joueur2.AfficherInfos();
+
+Console.WriteLine("\nDébut du jeu !");
+
+int CompteurTours = 0;
+bool jeuEnCours = true;
+
+while(jeuEnCours)
+
 {
-    internal class Program
+    CompteurTours++;
+
+
+    int LancerDeJoueur1 = Joueur.LancerDe();
+    Console.WriteLine($"{Joueur1.Nom} lance le De et obtient {LancerDeJoueur1}");
+    Joueur1.Deplacer(LancerDeJoueur1);
+    Console.WriteLine($"{Joueur1.Nom} est maintenant à la position : {Joueur1.Position}");
+    Thread.Sleep(1000);
+
+    if (Joueur1.Position == 50)
     {
-        static void Main(string[] args) 
-        {
-            Console.WriteLine("Entrez le nom du premier joureur");
-            string? InputJoueur1 = Console.ReadLine();
-            string NomJoueur1 = InputJoueur1 ?? "Joueur 1";
-
-            Console.WriteLine("Entrez le nom du deuxieme joureur");
-            string? InputJoueur2 = Console.ReadLine();
-            string NomJoueur2 = InputJoueur2 ?? "Joueur 2";
-
-            Joueur Joueur1 = new(NomJoueur1);
-            Joueur Joueur2 = new(NomJoueur2);
-
-            Console.WriteLine("\nJoueurs initialisés : ");
-            Joueur1.AfficherInfos();
-            Joueur2.AfficherInfos();
-        }
-
+        Console.WriteLine($"\n {Joueur1.Nom} a gagne la partie");
+        break;
     }
+
+    int LancerDeJoueur2 = Joueur.LancerDe();
+    Console.WriteLine($"{Joueur2.Nom} lance le De et obtient {LancerDeJoueur2}");
+    Joueur2.Deplacer(LancerDeJoueur2);
+    Console.WriteLine($"{Joueur2.Nom} est maintenant à la position : {Joueur2.Position}");
+    Thread.Sleep(1000);
+
+    if (Joueur2.Position == 50)
+    {
+        Console.WriteLine($"\n {Joueur2.Nom} a gagne la partie");
+        break;
+    }
+
+    if (CompteurTours > 100)
+    {
+        Console.WriteLine("\nLa partie est terminée après 100 tours sans vainqueur.");
+        break; 
+    }
+
+    Console.WriteLine(); 
 }
